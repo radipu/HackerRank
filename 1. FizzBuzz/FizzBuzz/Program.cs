fd304fb27bb0e288a71a59cd5e950b5cb0455db1
radipu <rad.edu.du@gmail.com>
2022-05-16 15:59:25 +0600

    solution of fizzbuzz

---
+++ 1. FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    public class Result
+    {
+        public static void fizzBuzz(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            Result.fizzBuzz(n);
+        }
+    }
+}
