ef1af182407cc545a6bac993837609dfa451a369
radipu <rad.edu.du@gmail.com>
2022-05-17 12:18:26 +0600

    solution of time conversion

---
+++ 4. TimeConversion/TimeConversion/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace TimeConversion
+{
+    public class Program
+    {
+        static string timeConversion(string s)
+        {
+            bool conversion = DateTime.TryParse(s, out DateTime time);
+            if (conversion)
+            {
+                return time.ToString("HH:mm:ss");
+            }
+
+            return "time conversion is done";
+        }
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+            string s = Console.ReadLine();
+            string result = Program.timeConversion(s);
+            textWriter.WriteLine(result);
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
