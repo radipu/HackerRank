7d833c62f80e1a0cb7f7e549b064c2d87ca6a581
radipu <rad.edu.du@gmail.com>
2022-05-25 15:14:54 +0600

    solution for Counting Sort 1

---
+++ 7. Counting Sort 1/Counting Sort 1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Counting_Sort_1
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+            int[] res = new int[100];
+            foreach (int i in Console.ReadLine().Trim().Split(' ').Select(int.Parse))
+            {
+                res[i]++;
+            }
+
+            foreach (int r in res)
+            {
+                Console.Write("{0} ", r);
+            }
+        }
+    }
+}
